4cff995e3e8605b41e6d37cddd83e1067bc4adb2
devanshuKushwah <devanshukushwah.001@gmail.com>
2021-04-23 00:58:14 +0530

    dond

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          devanshu kushwah
+          LEARN REACT
         </a>
       </header>
     </div>
