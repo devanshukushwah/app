d60c201603c6e46706ac4984d15d0e28b451a2b7
devanshuKushwah <devanshukushwah.001@gmail.com>
2021-04-23 20:48:12 +0530

    url check

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import CardNote from "./my-apps/CardNote";
 function App() {
-  return <h2>good</h2>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <h1>Good this is home page</h1>
+
+          <h4 style={{ display: "flex" }}>
+            <h2>Note: </h2> This is temporary! after next time, use direct
+            <a href="https://devanshukushwah.web.app/cardnote">
+              https://devanshukushwah.web.app/cardnote
+            </a>
+          </h4>
+          <Link to="/cardnote">CardNote</Link>
+        </Route>
+        <Route exact path="/cardnote">
+          <CardNote />
+        </Route>
+        <Route path="*">
+          <h1>Page not exists...</h1>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/my-apps/CardNote.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CardNote() {
+  return <h1>CardNote</h1>;
+}
+
+export default CardNote;
