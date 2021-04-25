1b839cff4eb2f7c72c50c188c64b1cbba0fa6be9
devanshuKushwah <devanshukushwah.001@gmail.com>
2021-04-25 15:03:14 +0530

    before folder -made

--- src/my-apps/CardNote/CardNote.js
+++ src/my-apps/CardNote/CardNote.js
@@ -2,46 +2,13 @@ import React, { useEffect, useState } from "react";
 import "./CardNote.css";
 import TopHeader from "./components/TopHeader";
 import CardsContainer from "./components/CardsContainer";
-import firebase from "./utils/firebase";
 
 function CardNote() {
-  const [listData, setListData] = useState([]);
   const [isOperation, setIsOperation] = useState(true);
-  const fetchdata = () => {
-    // setListData([]);
-    const ref = firebase.database().ref("cardnote/");
-    ref.on("value", (snapshot) => {
-      var tempList = [];
-      const data = snapshot.val();
-      for (let id in data) {
-        if (data[id].type === "folder") {
-          tempList.push(data[id]);
-        }
-        // tempList.push(data[id]);
-      }
-      for (let id in data) {
-        if (data[id].type === "card") {
-          tempList.push(data[id]);
-        }
-        // tempList.push(data[id]);
-      }
-
-      // setListData(set);
-      // console.log(set);
-      // const tempList2 = tempList.filter((item) => item.type === "folder");
-      // const tempList3 = tempList.filter((item) => item.type === "card");
-
-      setListData(tempList);
-      // console.log(listData);
-    });
-  };
-  useEffect(() => {
-    fetchdata();
-  }, [isOperation]);
   return (
     <>
       <TopHeader setIsOperation={setIsOperation} isOperation={isOperation} />
-      <CardsContainer listData={listData} />
+      <CardsContainer isOperation={isOperation} />
     </>
   );
 }
--- src/my-apps/CardNote/components/CardsContainer.js
+++ src/my-apps/CardNote/components/CardsContainer.js
@@ -1,17 +1,77 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./css/CardsContainer.css";
 import Card from "./Card";
 import Folder from "./Folder";
+import firebase from "../utils/firebase";
+import Loader from "./Loader";
 
-function CardsContainer({ listData }) {
-  return (
-    <main className="cards-container">
-      {listData.map((item, index) => {
-        if (item.type === "folder") {
-          return <Folder key={index} {...item} />;
+function CardsContainer({ isOperation }) {
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [listData, setListData] = useState(null);
+  const refContainer = useRef(null);
+  const fetchdata = () => {
+    const ref = firebase.database().ref("cardnote/");
+    ref.on("value", (snapshot) => {
+      var tempList = [];
+      const data = snapshot.val();
+      for (let id in data) {
+        if (data[id].type === "folder") {
+          tempList.push(data[id]);
         }
-        return <Card key={index} {...item} />;
-      })}
+      }
+      for (let id in data) {
+        if (data[id].type === "card") {
+          tempList.push(data[id]);
+        }
+      }
+      setListData(tempList);
+    });
+  };
+
+  // for updata innerwidth
+  const updateWidthAndHeight = () => {
+    setInnerWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWidthAndHeight);
+    return () => window.removeEventListener("resize", updateWidthAndHeight);
+  }, []);
+
+  useEffect(() => {
+    fetchdata();
+  }, [isOperation]);
+
+  // for grid height
+  useEffect(() => {
+    if (listData !== null) {
+      const lenData = listData.length;
+      if (lenData > 0) {
+        if (innerWidth < 510) {
+          refContainer.current.style.minHeight = `${120 * lenData}px`;
+        } else if (innerWidth > 509 && innerWidth < 730) {
+          refContainer.current.style.minHeight = `${(lenData / 2) * 120}px`;
+        } else if (innerWidth > 729 && innerWidth < 950) {
+          refContainer.current.style.minHeight = `${(lenData / 3) * 120}px`;
+        } else if (innerWidth > 949 && innerWidth < 1155) {
+          refContainer.current.style.minHeight = `${(lenData / 4) * 120}px`;
+        } else {
+          refContainer.current.style.minHeight = `${(lenData / 5) * 120}px`;
+        }
+      }
+    }
+  }, [listData, innerWidth]);
+
+  return (
+    <main className="cards-container" ref={refContainer}>
+      {!listData && <Loader />}
+      {listData &&
+        listData.map((item, index) => {
+          if (item.type === "folder") {
+            return <Folder key={index} {...item} />;
+          }
+          return <Card key={index} {...item} />;
+        })}
     </main>
   );
 }
